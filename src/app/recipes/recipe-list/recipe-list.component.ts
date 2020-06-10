15c907fb7c797cd519d72e1129410d39fdc656a9
Mamyan Manuk <manuk20061984@yandex.ru>
2020-06-10 21:45:49 +0300

    Recipe and nav
    Added Navigation(no routs yet) and worked on recipe page

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -14,8 +14,13 @@ export class RecipeListComponent implements OnInit {
       'https://cdn.pixabay.com/photo/2020/05/17/04/22/pizza-5179939_960_720.jpg'
     ),
   ];
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
